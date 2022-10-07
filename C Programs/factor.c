48b8bc53b7cf4f1890de25ee7fd855181c3b43f7
Abillou <82403488+Abillou@users.noreply.github.com>
2022-10-06 21:57:47 -0400

    Add files via upload

--- C Programs/factor.c
+++ C Programs/factor.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 int isPrime(int number){
 
     for(int j = 2; j <= number/2; j++){
@@ -12,34 +11,26 @@ int isPrime(int number){
     }
     return 1;
 }
+
 int main(int argc, char** argv){
 
     int a = atoi(argv[1]);
+    int primeFactorCount = 0;
+    int repeatPrimeFactor = 0;
     if(isPrime(a)){
         printf("%d\n", a);
     }
-
+   
     else{
 
-        for(int i = 2; i < a; i++){
-        
+        for(int i = 2; i <= a; i++){       
             if(a%i==0 && isPrime(i)){
                 printf("%d ", i);
-                if(isPrime(a/i)){
-                    printf("%d\n", a/i);
-                    break;
-                }
-                else{
-                    a/=i;
-                    while(a > 1){
-                        a/=i;
-                        printf("%d ", i);
-                    }
-                    printf("\n");
-                    break;
-                }
-            }   
+                a/=i;
+                i=1;
+            }           
         }
-
+        printf("\n");
     }   
+    
 }
